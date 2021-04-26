453bf150677e486021fceb46990d0c6f5b823299
Marcin <nowosielskimarcin@gmail.com>
2021-04-26 22:21:53 +0200

    Filling grid with largest squares possible around obstacles works!

--- grid_stuff.py
+++ grid_stuff.py
@@ -4,11 +4,15 @@ import numpy as np
 def fill_with_largest_rectangles(grid):
     """Fill grid with largest rectangles possible."""
     max_area = np.inf
+    filled_grid = grid.copy()
+    rectangle_count = 10
     while max_area > 1:
         max_area, max_row_stop, max_col_start, max_col_stop = find_largest_rectangle(grid)
-        n_rows = max_area // (max_col_stop - max_col_start + 1)
-        max_row_start = max_row_stop - n_rows + 1
-        grid[max_row_start:max_row_stop + 1, max_col_start:max_col_stop+1] = 0
+        n_rows = max_area // (max_col_stop - max_col_start)
+        max_row_start = max_row_stop - n_rows
+        grid[max_row_start:max_row_stop, max_col_start:max_col_stop] = 0
+        filled_grid[max_row_start:max_row_stop, max_col_start:max_col_stop] = rectangle_count
+        rectangle_count += 1
 
     return
 
@@ -25,7 +29,7 @@ def find_largest_rectangle(grid):
         area, area_col_start, area_col_stop = largest_area_under_histogram(current_row)
         if area > max_area:
             max_area = area
-            max_row_stop = row_id
+            max_row_stop = row_id + 1
             max_col_start = area_col_start
             max_col_stop = area_col_stop
 
@@ -59,10 +63,12 @@ def largest_area_under_histogram(histogram):
             if area == max_area:
                 if stack:
                     max_col_start = top_of_stack
-                    max_col_stop = max_col_start + index - stack[-1] - 2
+                    max_col_stop = max_col_start + index - stack[-1] - 1
                 else:
-                    max_col_start = top_of_stack
-                    max_col_stop = max_col_start + index - 2
+                    max_col_start = 0
+                    max_col_stop = index
+                    # max_col_start = top_of_stack
+                    # max_col_stop = max_col_start + index
     while stack:
         top_of_stack = stack.pop()
         area = (histogram[top_of_stack] * ((index - stack[-1] - 1) if stack else index))
@@ -70,9 +76,11 @@ def largest_area_under_histogram(histogram):
         if area == max_area:
             if stack:
                 max_col_start = top_of_stack
-                max_col_stop = max_col_start + index - stack[-1] - 2
+                max_col_stop = max_col_start + index - stack[-1] - 1
             else:
-                max_col_start = top_of_stack
-                max_col_stop = max_col_start + index - 2
+                max_col_start = 0
+                max_col_stop = index
+                # max_col_start = top_of_stack
+                # max_col_stop = max_col_start + index
 
     return max_area, max_col_start, max_col_stop
--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import os
 import pickle
 import snake
 
-print(largest_area_under_histogram(np.array([1,12,12,3,0,0])))
-print(largest_area_under_histogram(np.array([
-    12,12,3])))
+# print(largest_area_under_histogram(np.array([1,12,12,3,0,0])))
+# print(largest_area_under_histogram(np.array([12,12,3])))
+# print(largest_area_under_histogram(np.array([1, 2, 3, 4])))
+# print(largest_area_under_histogram(np.array([1, 6, 6, 6])))
+# print(largest_area_under_histogram(np.array([2, 1, 0, 0, 0])))
 an_array_to_sum = np.full([4, 5], fill_value=1)
 an_array_to_sum[:2, 2:] = 0
 an_array_to_sum[2, 1] = 0
-# print(fill_with_largest_rectangles(an_array_to_sum))
+print(fill_with_largest_rectangles(an_array_to_sum))
 
 # snake_game = snake.SnakeGame(display_width=400, display_height=440, snake_speed=50, ai_mode='if_statement')
 # bsf_on_a_grid = pickle.load(open('bsf.pkl', 'rb'))
