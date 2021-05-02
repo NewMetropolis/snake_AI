04fe1f252f2ed6bcad22f83085d89c3c86414a6e
Marcin <nowosielskimarcin@gmail.com>
2021-05-02 14:35:50 +0200

    New algorithm for finding articulation points.

--- graph_stuff.py
+++ graph_stuff.py
@@ -248,113 +248,64 @@ def escape_trap_2(colored_grid, snake, reachable):
     return
 
 
-class DepthFirstSearch:
-    """Depth First Search on a grid."""
+def find_articulation_points(grid, start):
+    """Find articulation points on a grid using Depth First Search based method."""
+    ids_for_inspection = grid.copy()
+    lowpoint_for_inspection = grid.copy()
+    n = grid.size
+    visited = np.full([n], fill_value=False)
+    node_id = np.full([n], fill_value=-1, dtype=int)
+    lowpoint = np.full([n], fill_value=np.inf)
+    parent = np.full([n], fill_value=-1, dtype=int)
+    flatten = grid.flatten()
+    start_f = np.ravel_multi_index(start, grid.shape)
+    n_cols = grid.shape[1]
+    allowed_moves = [1, -n_cols, -1, n_cols]
+    node_stack = [start_f]
+    backtrack_list = [start_f]
+    current_id = 0
+    # end_f = np.ravel_multi_index(end, grid.shape)
+    # Precompute heuristics.
+    # indexing_2d = np.indices(grid.shape)
 
-    def __init__(self, grid, start, end=None, snake_body=None):
-        self.grid = grid
-        # Number of rows.
-        self.n_rows = grid.shape[0]
-        # Number of columns.
-        self.n_columns = grid.shape[1]
-        # Node to start at.
-        self.start = start
-        # End node.
-        self.end = end
-        # Allowed row index changes/moves.
-        self.r_moves = [0, -1, 0, 1]
-        # Allowed column index changes/moves.
-        self.c_moves = [1, 0, -1, 0]
-        # Node depth.
-        self.depth = np.full([self.n_rows, self.n_columns], fill_value=np.nan)
-        # ID of the lowest reachable node.
-        self.lowpoint = np.full([self.n_rows, self.n_columns], fill_value=np.nan)
+    while node_stack:
+        flat_index = node_stack.pop()
+        if not visited[flat_index]:
+            visited[flat_index] = True
+            node_id[flat_index] = current_id
+            lowpoint[flat_index] = current_id
+            lowpoint_for_inspection[np.unravel_index(flat_index, grid.shape)] = lowpoint[flat_index]
+            ids_for_inspection[np.unravel_index(flat_index, grid.shape)] = node_id[flat_index]
+            current_id += 1
 
-        # # This will keep track a route.
-        # self.previous_node = np.full(self.n_rows * self.n_columns, -1, dtype=int)
-        # # Can be used for navigating Snake in a game.
-        self.snake = snake_body
-        # self.track = []
+        # position_2d = np.unravel_index(node_id, grid.shape)
+        # grid_for_inspection[position_2d] = -3
+        for index_change in allowed_moves:
 
-        return
+            new_flat_index = flat_index + index_change
 
-    def search(self):
-        # A queue for holding a row index.
-        r_queue = [self.start[0]]
-        # A queue for holding a column index.
-        c_queue = [self.start[1]]
-        # Avoid duplicates in a queue.
-        enqueued = [0] * self.grid.size
-        # For counting number of steps.
-        nodes_left_in_layer = 1
-        nodes_in_next_layer = 0
-        move_count = 0
-        while len(r_queue) > 0:
-            # Current row index.
-            r = r_queue.pop(0)
-            # Current column index.
-            c = c_queue.pop(0)
-            node_index = r * self.n_columns + c
-            # Mark as visited/now unreachable.
-            self.grid[r, c] = 1
-            # Explore all possible moves.
-            for ith_direction in range(4):
-                r_change = self.r_moves[ith_direction]
-                c_change = self.c_moves[ith_direction]
-                new_r = r + r_change
-                new_c = c + c_change
-                new_node_index = new_r * self.n_columns + new_c
-                # Still on a grid?
-                if new_r < 0 or new_c < 0:
-                    continue
-                if new_r >= self.n_rows or new_c >= self.n_columns:
-                    continue
-                # Any obstacles\cell already visited
-                if self.grid[new_r, new_c] == 1:
-                    continue
+            if new_flat_index < 0 or new_flat_index >= n:
+                continue
+            elif flatten[new_flat_index] == 0:
+                continue
+            elif index_change == 1 and new_flat_index % n_cols == 0:
+                continue
+            elif index_change == -1 and flat_index % n_cols == 0:
+                continue
+            if new_flat_index == parent[flat_index]:
+                continue
+            if visited[new_flat_index]:
+                lowpoint[flat_index] = min(lowpoint[flat_index], node_id[new_flat_index])
+                lowpoint_for_inspection[np.unravel_index(flat_index, grid.shape)] = lowpoint[flat_index]
+            else:
+                lowpoint[flat_index] = min(lowpoint[flat_index], lowpoint[new_flat_index])
+                lowpoint_for_inspection[np.unravel_index(flat_index, grid.shape)] = lowpoint[flat_index]
+                parent[new_flat_index] = flat_index
+                node_stack.append(new_flat_index)
 
-                # Keep track of a route.
-                self.previous_node[new_node_index] = node_index
-                # Have we already reached an end?
+        while  para
 
-                if enqueued[new_node_index] == 1:
-                    continue
-                # Add to a queue.
-                r_queue.append(new_r)
-                c_queue.append(new_c)
-                enqueued[new_node_index] = 1
-                # Keep track of how many nodes are to be visited in next layer.
-                nodes_in_next_layer += 1
-            nodes_left_in_layer -= 1
-            # Did we exit a current layer yet?
-            if nodes_left_in_layer == 0:
-                nodes_left_in_layer = nodes_in_next_layer
-                nodes_in_next_layer = 0
-                move_count += 1
-                if self.snake:
-                    # Place where the Snake's tail previously was is now empty.
-                    if len(self.snake) > 0 and move_count > 1:
-                        index_to_empty = tuple(self.snake.pop())
-                        self.grid[index_to_empty] = 1
+    return
 
-        return
 
-    def reconstruct_track(self):
-        # (Re)construct a path given a 'previous node' list from 'bfs_on_grid' function.
-        n_columns = self.n_columns
-        start = n_columns * self.start[0] + self.start[1]
-        end = n_columns * self.end[0] + self.end[1]
-        node = end
-        if self.previous_node[node] == -1:
-            print('No valid path.')
 
-            return
-
-        while node != start:
-            row_idx = node // n_columns
-            col_idx = node % n_columns
-            self.track.append(np.array([row_idx, col_idx]))
-            node = self.previous_node[node]
-        self.track.reverse()
-
-        return 1
